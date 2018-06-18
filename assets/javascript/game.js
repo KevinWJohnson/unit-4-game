d46d7be00aaa2043fef4af32868304bef2c9b6d4
KevinWJohnson <kevin.johnson3@cox.net>
2018-06-18 16:42:12 -0700

    Update 6/18/2018 4:42pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,61 @@ $(document).ready(function () {
     var totalScore = 0;
     var targetNumber = 0;
 
-    // Getting random numbers for crystals
-    var numberOptions = [0, 0, 0, 0];
+    var crystal = {
+        crystal_1: {
+            points: 0,
+            imgLoc: "https://www.ahbeads.com/productimages/300/14152071.png"
+        },
 
-    var pictureLoc = [
-        "https://www.ahbeads.com/productimages/300/14152071.png",
-        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/8/8d/Daily_Crystal.png/revision/latest?cb=20150825213624",
-        "https://vignette.wikia.nocookie.net/the-secret-society/images/e/eb/The_Legend_Returns_Jade_Crystal_special_items.PNG/revision/latest?cb=20170205145851",
-        "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/8/84/Crystal_multi_science.png/revision/latest?cb=20151121235753"
-    ];
+        crystal_2: {
+            points: 0,
+            imgLoc: "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/8/8d/Daily_Crystal.png/revision/latest?cb=20150825213624"
+        },
 
+        crystal_3: {
+            points: 0,
+            imgLoc: "https://vignette.wikia.nocookie.net/the-secret-society/images/e/eb/The_Legend_Returns_Jade_Crystal_special_items.PNG/revision/latest?cb=20170205145851"
+        },
 
-    function rmImages() {
-        var remove = document.getElementsByClassName("crystal-image");
-
-        // As long as Class has a child node, remove it
-        while (remove.hasChildNodes()) {
-            remove.removeChild(remove.firstChild);
+        crystal_4: {
+            points: 0,
+            imgLoc: "https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/8/84/Crystal_multi_science.png/revision/latest?cb=20151121235753"
         }
+
+    }
+
+    function randomTarget() {
+        return Math.floor(Math.random() * 120) + 19; // returns a number between 19 and 120
+    }
+
+    function resetValues() {
+
+        // Setting the total score to 0
+        totalScore = 0;
+
+        // Getting random target number
+        targetNumber = randomTarget();
+
+        // Getting random crystal numbers
+        crystal.crystal_1.points = Math.floor(Math.random() * 12) + 1; // returns a number between 1 and 12
+        crystal.crystal_2.points = Math.floor(Math.random() * 12) + 1; // returns a number between 1 and 12
+        crystal.crystal_3.points = Math.floor(Math.random() * 12) + 1; // returns a number between 1 and 12
+        crystal.crystal_4.points = Math.floor(Math.random() * 12) + 1; // returns a number between 1 and 12
+
+        console.log("crystal_1 points: " + crystal.crystal_1.points)
+        console.log("crystal_2 points: " + crystal.crystal_2.points)
+        console.log("crystal_3 points: " + crystal.crystal_3.points)
+        console.log("crystal_4 points: " + crystal.crystal_4.points)
+
+
     }
 
     // Creating crystals for every numberOption
 
-    function newCrystals() {
+    function renderCrystals() {
 
 
-        for (var i = 0; i < numberOptions.length; i++) {
+        for (var key in crystal) {
 
             // For each iteration, creating an imageCrystal
             imageCrystal = $("<img>");
@@ -44,7 +73,8 @@ $(document).ready(function () {
             imageCrystal.addClass("img-responsive");
 
             // Each crystal will be given a scr link to the crystal image
-            imageCrystal.attr("src", pictureLoc[i]);
+            imageCrystal.attr("src", crystal[key].imgLoc);
+            console.log("imgLoc " + crystal[key].imgLoc);
 
             //console.log(imageCrystal.attr("src", pictureLoc[i]))
 
@@ -52,10 +82,14 @@ $(document).ready(function () {
             // Each imageCrystal will be given a data attribute called data-crystalValue.
             // This data attribute will be set equal to the array value.
 
-            imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+            imageCrystal.attr("data-crystalvalue", crystal[key].points);
+            console.log("key.points " + crystal[key].points);
 
             // Each crystal image (with all its classes and attributes) will get added to
-
+            
+            // Commands to empty the area before the recreation (append)
+            $("#crystals").html();
+            $("#crystals").empty();
             $("#crystals").append(imageCrystal);
 
             // Displaying the results
@@ -64,40 +98,7 @@ $(document).ready(function () {
 
     }
 
-    function reruncrystalClick() {
-
-
-        // Getting random target number
-        targetNumber = randomTarget();
-
-
-        function randomTarget() {
-            return Math.floor(Math.random() * 120) + 19; // returns a number between 19 and 120
-        }
-
-
-        // Getting random crystal numbers
-        numberOptions = randomCrystal(numberOptions);
-
-
-        function randomCrystal(randCrytalsArray) {
-            for (var i = 0; i < randCrytalsArray.length; i++) {
-                randCrytalsArray[i] = Math.floor(Math.random() * 12) + 1; // returns a number between 1 and 12
-            }
-            return randCrytalsArray
-        }
-
-        for (var i = 0; i < numberOptions.length; i++) {
-            console.log(numberOptions[i]);
-        }
-
-
-        // Updating the imageCrystal attribute with new numberOptions
-        //for (var i = 0; i < numberOptions.length; i++) {
-        //    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        //}
-        newCrystals();
-
+    function updateScore() {
         // Click event applies to every single crystal on the page.
 
         $(".crystal-image").on("click", function () {
@@ -110,32 +111,37 @@ $(document).ready(function () {
             // Each time a crystal is selected it's value is added to the counter
             totalScore += crystalValue;
             $("#scoreTot").text(totalScore);
-            console.log(crystalValue)
-
-            // Win-Lose logic of the game
-
-            // You win
-            if (totalScore === targetNumber) {
-                winTotal++;
-                totalScore = 0;
-                $("#winLoseMessage").text("You Win!");
-                // clearing the old crystals
-                rmImages();
-                reruncrystalClick();
-            }
-            // You lose
-            else if (totalScore >= targetNumber) {
-                lossTotal++;
-                totalScore = 0;
-                $("#winLoseMessage").text("You Lose!");
-                // clearing the old crystals
-                rmImages();
-                reruncrystalClick();
-            }
-
+            console.log(crystalValue);
         });
-
     }
+
+
+
+    function winLose(totalScore) {
+
+
+        // Win-Lose logic of the game
+
+        // You win
+        if (totalScore === targetNumber) {
+            winTotal++;
+            totalScore = 0;
+            $("#winLoseMessage").text("You Win!");
+            // clearing the old crystals
+            resetValues();
+        }
+        // You lose
+        else if (totalScore >= targetNumber) {
+            lossTotal++;
+            totalScore = 0;
+            $("#winLoseMessage").text("You Lose!");
+            // clearing the old crystals
+            resetValues();
+        }
+    }
+
+
+
 
     function displayResults() {
 
@@ -149,9 +155,14 @@ $(document).ready(function () {
         $("#scoreTot").text(totalScore);
 
     }
+    // Game function calls
+
+
 
     // For the first time the program appears
-    reruncrystalClick();
     displayResults();
-
+    resetValues();
+    renderCrystals();
+    updateScore();
+    
 });
